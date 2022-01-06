3b73836e69059ddea67f920728fd1b07ed9ce876
Cedric Shock <cedricshock@gmail.com>
2022-01-05 18:24:05 -0700

    Webpack progressive web app

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: '/contrastvisor/index.js',
@@ -11,6 +12,12 @@ module.exports = {
     new HtmlWebpackPlugin({
      title: 'Progressive Web Application',
      template: 'contrastvisor/index.html'
+    }),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
     }),
   ],
   module: {
