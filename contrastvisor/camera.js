2aaaa88a14a62a5997d4e588c7d4dbc6f1a18cf3
Cedric Shock <cedricshock@gmail.com>
2022-01-20 09:27:21 -0700

    React Camera component
    * Converted camera component to react
    * Promise serializer that approximates a behaviour from a series of promises
    * Proof of concept of controlling camera constraints

--- contrastvisor/camera.js
+++ contrastvisor/camera.js
@@ -1,101 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useCallback, useRef, useState, } from 'react';
+import useDeepCompareEffect from 'use-deep-compare-effect'
+import { PromiseSerializer } from './promises.js'
 
-function videoCamera() {
-  return <video playsInline autoPlay style={{display: 'none'}}></video>
-}
 
-export class Camera {
-
-  constructor() {
-    this.onError = console.log;
-
-    this.container = document.body.appendChild(document.createElement("div"));
-    this.current = null;
-    this.currentFrame = null;
-    this.mockFrame = null;
-
-    this.changing = false;
-    this.nextConstraints = null;
-
-    this.callbacks = [];
-  }
-
-  requestVideoFrameCallback(callback) {
-    this.callbacks.push(callback);
-  }
-
-  videoFrameCallback(now, frame) {
-    this.currentFrame = frame;
-    var callbacks = this.callbacks;
-    this.callbacks = [];
-    callbacks.forEach(c => c(now, frame));
-    this.watch();
-  }
-
-  watch() {
-    if (this.current) {
-      if (this.current.requestVideoFrameCallback) {
-        this.current.requestVideoFrameCallback((n, f) => this.videoFrameCallback(n, f));
-      } else {
-        requestAnimationFrame(now => this.videoFrameCallback(now, this.mockFrame))
-      }
-    }
-  }
-
-  change(constraints) {
-    this.nextConstraints = constraints;
-    if (!this.changing) {
-      this.startChange();
-    }
-  }
-
-  startChange() {
-    this.changing = true;
-    const constraints = this.nextConstraints;
-    this.nextConstraints = null;
-
-    navigator.mediaDevices.getUserMedia(constraints).then(s => this.setStream(s)).catch(this.onError).then(() => {
-      this.changing = false;
-      if (this.nextConstraints) {
-        this.startChange();
-      }
+function stopStream(stream) {
+  if (stream) {
+    stream.getTracks().forEach(track => {
+      stream.removeTrack(track);
+      track.stop()
     });
   }
+}
 
-  setStream(stream) {
-    // changing the srcObject on a video element doesn't work
-    this.currentFrame = null;
-    this.mockFrame = null;
-    if (this.current) {
-      this.current.srcObject.getTracks().forEach(track => track.stop());
-      this.current.srcObject = null;
-    }
-    this.current = null;
-    while (this.container.firstChild) {
-      this.container.removeChild(this.container.firstChild);
-    }
-    ReactDOM.unmountComponentAtNode(this.container);
-    const camera = ReactDOM.render(videoCamera(), this.container);
+
+export function Camera({ constraints, onFrame }) {
+
+  const camera = useRef();
+  const frameHandler = useRef();
+  frameHandler.current = onFrame;
+  const [streamManager] = useState(PromiseSerializer);
+
+  useDeepCompareEffect(() => {
+    // Start a new stream
+    var streamStarted = navigator.mediaDevices.getUserMedia(constraints);
+    var streamDone = streamManager(streamStarted, stream => {
+      camera.current.srcObject = stream;
+    });
+    streamDone.then(stopStream);
+  }, [constraints]);
+
+  // Start watching the camera
+  const camRef = useCallback(node => {
+    camera.current = node;
     
-    camera.onloadedmetadata = e => {
-      this.mockFrame = {
-        width: camera.videoWidth,
-        height: camera.videoHeight
+    function videoFrameCallback(now, frame) {
+      if (camera.current != node) {
+        return;
       }
-      console.log([camera.videoWidth, camera.videoHeight, "Camera"]);
+      try {
+        if (frameHandler.current) {
+          frameHandler.current(now, frame, node);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+      watch();
     }
 
-    camera.srcObject = stream;
-    this.current = camera;
-
-    this.watch();
-  }
-
-  capture(gl, texture) {
-    if (this.currentFrame) {
-      gl.bindTexture(gl.TEXTURE_2D, texture);
-      gl.texImage2D(gl.TEXTURE_2D, 0,  gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.current);
+    function watch() {
+      node.requestVideoFrameCallback(videoFrameCallback);
     }
-  }
+    watch();
+  }, []);
+
+  return <video ref={camRef} playsInline autoPlay style={{ display: 'none' }}></video>
 }
